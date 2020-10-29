4d58fac5068531fe4686fcd78c9b894ed5fe93eb
0xsaxit <bcsaxit@gmail.com>
2020-10-29 16:30:05 +0700

    dgSlotsFlat moved (to flats folder)

--- flats/dgSlotsFlat.sol
+++ flats/dgSlotsFlat.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: -- 🎲 --
 
-pragma solidity ^0.7.0;
+pragma solidity ^0.7.4;
 
 // Slot Machine Logic Contract ///////////////////////////////////////////////////////////
 // Author: Decentral Games (hello@decentral.games) ///////////////////////////////////////
@@ -166,12 +166,49 @@ interface TreasuryInstance {
     ) external returns (bool);
 }
 
-contract TreasurySlots is AccessController {
+interface PointerInstance {
+
+    function addPoints(
+        address _player,
+        uint256 _points,
+        address _token,
+        uint256 _numPlayers,
+        uint256 _wearableBonus
+    ) external returns (
+        uint256 newPoints,
+        uint256 multiplierA,
+        uint256 multiplierB
+    );
+
+    function addPoints(
+        address _player,
+        uint256 _points,
+        address _token,
+        uint256 _numPlayers
+    ) external returns (
+        uint256 newPoints,
+        uint256 multiplierA,
+        uint256 multiplierB
+    );
+
+    function addPoints(
+        address _player,
+        uint256 _points,
+        address _token
+    ) external returns (
+        uint256 newPoints,
+        uint256 multiplierA,
+        uint256 multiplierB
+    );
+}
+
+contract dgSlots is AccessController {
 
     using SafeMath for uint128;
 
     uint256 private factors;
     TreasuryInstance public treasury;
+    PointerInstance public pointerContract;
 
     event GameResult(
         address _player,
@@ -187,9 +224,13 @@ contract TreasurySlots is AccessController {
         uint16 factor1,
         uint16 factor2,
         uint16 factor3,
-        uint16 factor4
-    ) {
-        treasury = TreasuryInstance(_treasury);
+        uint16 factor4,
+        address _pointerAddress
+        )
+    {
+        treasury = TreasuryInstance(
+            _treasury
+        );
 
         require(
             factor1 > factor2 + factor3 + factor4,
@@ -200,6 +241,10 @@ contract TreasurySlots is AccessController {
         factors |= uint256(factor2)<<16;
         factors |= uint256(factor3)<<32;
         factors |= uint256(factor4)<<48;
+
+        pointerContract = PointerInstance(
+            _pointerAddress
+        );
     }
 
     function play(
@@ -208,9 +253,13 @@ contract TreasurySlots is AccessController {
         uint128 _machineID,
         uint128 _betAmount,
         bytes32 _localhash,
-        uint8 _tokenIndex
-    ) public whenNotPaused onlyWorker {
-
+        uint8 _tokenIndex,
+        uint256 _wearableBonus
+    )
+        public
+        whenNotPaused
+        onlyWorker
+    {
         require(
             treasury.checkApproval(_player, _tokenIndex) >= _betAmount,
             'Slots: exceeded allowance amount'
@@ -234,6 +283,14 @@ contract TreasurySlots is AccessController {
 
         treasury.consumeHash(
            _localhash
+        );
+
+        pointerContract.addPoints(
+            _player,
+            _betAmount,
+            treasury.getTokenAddress(_tokenIndex),
+            1,
+            _wearableBonus
         );
 
         (uint256 _number, uint256 _winAmount) = _launch(
@@ -262,10 +319,14 @@ contract TreasurySlots is AccessController {
     function _launch(
         bytes32 _localhash,
         uint128 _betAmount
-    ) internal view returns (
-        uint256 number,
-        uint256 winAmount
-    ) {
+    )
+        internal
+        view
+        returns (
+            uint256 number,
+            uint256 winAmount
+        )
+    {
         number = getRandomNumber(_localhash) % 1000;
         uint256 _numbers = number;
 
@@ -293,7 +354,11 @@ contract TreasurySlots is AccessController {
 
     function getRandomNumber(
         bytes32 _localhash
-    ) private pure returns (uint256) {
+    )
+        private
+        pure
+        returns (uint256)
+    {
         return uint256(
             keccak256(
                 abi.encodePacked(
@@ -305,15 +370,23 @@ contract TreasurySlots is AccessController {
 
     function getPayoutFactor(
         uint8 _position
-    ) external view returns (uint16) {
-       return uint16(
+    )
+        external
+        view
+        returns (uint16)
+    {
+        return uint16(
            factors>>_position
         );
     }
 
     function getMaxPayout(
         uint128 _betSize
-    ) public view returns (uint256) {
+    )
+        public
+        view
+        returns (uint256)
+    {
         return _betSize.mul(
             uint16(
                 factors>>0
@@ -326,8 +399,10 @@ contract TreasurySlots is AccessController {
         uint16 factor2,
         uint16 factor3,
         uint16 factor4
-    ) external onlyCEO {
-
+    )
+        external
+        onlyCEO
+    {
         require(
             factor1 > factor2 + factor3 + factor4,
             'Slots: incorrect ratio'
@@ -343,19 +418,21 @@ contract TreasurySlots is AccessController {
 
     function updateTreasury(
         address _newTreasuryAddress
-    ) external onlyCEO {
+    )
+        external
+        onlyCEO
+    {
         treasury = TreasuryInstance(
             _newTreasuryAddress
         );
     }
 
-    function migrateTreasury(
-        address _newTreasuryAddress
-    ) external {
-        require(
-            msg.sender == address(treasury),
-            'Slots: wrong treasury address'
+    function updatePointer(address _newPointerAddress)
+        external
+        onlyCEO
+    {
+         pointerContract = PointerInstance(
+            _newPointerAddress
         );
-        treasury = TreasuryInstance(_newTreasuryAddress);
     }
 }
