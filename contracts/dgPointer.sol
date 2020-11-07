6967b3d2bdfa2926ec1feacf063dd2f8fe534536
0xsaxit <bcsaxit@gmail.com>
2020-11-08 02:22:54 +0700

    dgPointer -> executeMetaTransaction updated

--- contracts/dgPointer.sol
+++ contracts/dgPointer.sol
@@ -7,9 +7,16 @@ import "./common-contracts/AccessController.sol";
 import "./common-contracts/ERC20Token.sol";
 import "./common-contracts/EIP712Base.sol";
 
-abstract contract ExecuteMetaTransaction is EIP712Base {
+abstract contract EIP712MetaTransaction is EIP712Base {
 
     using SafeMath for uint256;
+
+    bytes32 private constant META_TRANSACTION_TYPEHASH =
+        keccak256(
+            bytes(
+                "MetaTransaction(uint256 nonce,address from,bytes functionSignature)"
+            )
+        );
 
     event MetaTransactionExecuted(
         address userAddress,
@@ -17,88 +24,18 @@ abstract contract ExecuteMetaTransaction is EIP712Base {
         bytes functionSignature
     );
 
-    bytes32 internal constant META_TRANSACTION_TYPEHASH = keccak256(
-        bytes(
-            "MetaTransaction(uint256 nonce,address from,bytes functionSignature)"
-        )
-    );
-
     mapping(address => uint256) internal nonces;
 
+    /*
+     * Meta transaction structure.
+     * No point of including value field here as if user is doing value transfer then he has the funds to pay for gas
+     * He should call the desired function directly in that case.
+     */
     struct MetaTransaction {
 		uint256 nonce;
 		address from;
         bytes functionSignature;
 	}
-
-    function getNonce(
-        address user
-    )
-        public
-        view
-        returns(uint256 nonce)
-    {
-        nonce = nonces[user];
-    }
-
-    function verify(
-        address signer,
-        MetaTransaction memory metaTx,
-        bytes32 sigR,
-        bytes32 sigS,
-        uint8 sigV
-    ) internal view returns (bool) {
-        return
-            signer ==
-            ecrecover(
-                toTypedMessageHash(hashMetaTransaction(metaTx)),
-                sigV,
-                sigR,
-                sigS
-            );
-    }
-
-    function hashMetaTransaction(
-        MetaTransaction memory metaTx
-    )
-        internal
-        pure
-        returns (bytes32)
-    {
-        return
-            keccak256(
-                abi.encode(
-                    META_TRANSACTION_TYPEHASH,
-                    metaTx.nonce,
-                    metaTx.from,
-                    keccak256(metaTx.functionSignature)
-                )
-            );
-    }
-
-    function msgSender()
-        internal
-        view
-        returns(address sender)
-    {
-        if (msg.sender == address(this)) {
-
-            bytes memory array = msg.data;
-            uint256 index = msg.data.length;
-            assembly {
-                sender := and(
-                    mload(add(array, index)),
-                    0xffffffffffffffffffffffffffffffffffffffff
-                )
-            }
-
-        } else {
-
-            sender = msg.sender;
-
-        }
-        return sender;
-    }
 
     function executeMetaTransaction(
         address userAddress,
@@ -106,34 +43,44 @@ abstract contract ExecuteMetaTransaction is EIP712Base {
         bytes32 sigR,
         bytes32 sigS,
         uint8 sigV
-    ) public returns (bytes memory) {
-
-        MetaTransaction memory metaTx = MetaTransaction({
-            nonce: nonces[userAddress],
-            from: userAddress,
-            functionSignature: functionSignature
-        });
-
-        require(
-            verify(userAddress, metaTx, sigR, sigS, sigV),
-            'Signer and signature do not match'
+    )
+        public
+        payable
+        returns(bytes memory)
+    {
+        MetaTransaction memory metaTx = MetaTransaction(
+            {
+                nonce: nonces[userAddress],
+                from: userAddress,
+                functionSignature: functionSignature
+            }
         );
 
+        require(
+            verify(
+                userAddress,
+                metaTx,
+                sigR,
+                sigS,
+                sigV
+            ), "Signer and signature do not match"
+        );
+
+	    nonces[userAddress] =
+	    nonces[userAddress].add(1);
+
+        // Append userAddress at the end to extract it from calling context
         (bool success, bytes memory returnData) = address(this).call(
             abi.encodePacked(
                 functionSignature,
-                userAddress,
-                msg.sender
+                userAddress
             )
         );
 
         require(
             success,
-            'dgPointer: Function call not successfull'
+            'Function call not successful'
         );
-
-        nonces[userAddress] =
-        nonces[userAddress] + 1;
 
         emit MetaTransactionExecuted(
             userAddress,
@@ -143,9 +90,77 @@ abstract contract ExecuteMetaTransaction is EIP712Base {
 
         return returnData;
     }
+
+    function hashMetaTransaction(
+        MetaTransaction memory metaTx
+    )
+        internal
+        pure
+        returns (bytes32)
+    {
+		return keccak256(
+		    abi.encode(
+                META_TRANSACTION_TYPEHASH,
+                metaTx.nonce,
+                metaTx.from,
+                keccak256(metaTx.functionSignature)
+            )
+        );
+	}
+
+    function getNonce(
+        address user
+    )
+        external
+        view
+        returns(uint256 nonce)
+    {
+        nonce = nonces[user];
+    }
+
+    function verify(
+        address user,
+        MetaTransaction memory metaTx,
+        bytes32 sigR,
+        bytes32 sigS,
+        uint8 sigV
+    )
+        internal
+        view
+        returns (bool)
+    {
+        address signer = ecrecover(
+            toTypedMessageHash(
+                hashMetaTransaction(metaTx)
+            ),
+            sigV,
+            sigR,
+            sigS
+        );
+
+        require(
+            signer != address(0x0),
+            'Invalid signature'
+        );
+		return signer == user;
+	}
+
+    function msgSender() internal view returns(address sender) {
+        if(msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(mload(add(array, index)), 0xffffffffffffffffffffffffffffffffffffffff)
+            }
+        } else {
+            sender = msg.sender;
+        }
+        return sender;
+    }
 }
 
-contract dgPointer is AccessController, ExecuteMetaTransaction {
+contract dgPointer is AccessController, EIP712MetaTransaction {
 
     using SafeMath for uint256;
 
