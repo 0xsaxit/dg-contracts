c1f800845915b898e8ffa65b93c2482f8b993db3
0xsaxit <bcsaxit@gmail.com>
2020-11-08 04:19:24 +0700

    dgKeeper (pre-production version)

--- contracts/dgKeeper.sol
+++ contracts/dgKeeper.sol
@@ -1,81 +1,112 @@
 // SPDX-License-Identifier: -- 🎲 --
 
-pragma solidity ^0.7.0;
+pragma solidity ^0.7.4;
 
 import "./common-contracts/AccessController.sol";
 import "./common-contracts/ERC20Token.sol";
+import "./common-contracts/SafeMath.sol";
 
-contract dgAirdrop is AccessController {
+contract dgKeeper is AccessController {
 
-    bool public distributionEnabled;
+    using SafeMath for uint256;
+
     ERC20Token public distributionToken;
 
-    mapping(address => uint256) public airdropBalance;
+    mapping(address => uint256) public keeperRate;
+    mapping(address => uint256) public keeperDate;
+    mapping(address => uint256) public keeperBalance;
+    mapping(address => uint256) public keeperPayouts;
 
     constructor(address _distributionToken) {
-        distributionToken = ERC20Token(_distributionToken);
+        distributionToken = ERC20Token(
+            _distributionToken
+        );
     }
 
     function allocateTokensBulk(
         address[] memory _recipients,
-        uint256[] memory _tokens
+        uint256[] memory _tokensOpened,
+        uint256[] memory _tokensLocked,
+        uint256[] memory _timeFrame
     )
         external
         onlyWorker
     {
         for(uint i = 0; i < _recipients.length; i++) {
-            allocateTokens(_recipients[i], _tokens[i]);
+            allocateTokens(
+                _recipients[i],
+                _tokensOpened[i],
+                _tokensLocked[i],
+                _timeFrame[i]
+            );
         }
     }
 
     function allocateTokens(
         address _recipient,
-        uint256 _tokens
+        uint256 _tokensOpened,
+        uint256 _tokensLocked,
+        uint256 _timeFrame
     )
         public
         onlyWorker
     {
-        airdropBalance[_recipient] = _tokens;
+        require(
+            _timeFrame > 0,
+            'dgKeeper: undefined timeFrame'
+        );
+
+        keeperDate[_recipient] = getNow();
+        keeperRate[_recipient] = _tokensLocked.div(_timeFrame);
+        keeperBalance[_recipient] = _tokensOpened;
     }
 
-    function getMyTokens() external returns(uint256 tokenAmount) {
-        return distributeTokens(msg.sender);
+    function scrapeTokens()
+        public
+    {
+        uint256 _availableBalance =
+        availableBalance(msg.sender);
+
+        keeperPayouts[msg.sender] =
+        keeperPayouts[msg.sender].add(_availableBalance);
+
+        if (keeperBalance[msg.sender] > 0) {
+            keeperBalance[msg.sender] = 0;
+        }
+
+        ERC20Token t = ERC20Token(distributionToken);
+        t.transfer(msg.sender, _availableBalance);
     }
 
-    function distributeTokensBulk(
-        address[] memory _recipients
+    function availableBalance(address _user)
+        public
+        view
+        returns (uint256 rewardAvailable)
+    {
+        uint256 timePassed = getNow().sub(keeperDate[_user]);
+        rewardAvailable = keeperRate[_user]
+            .mul(timePassed)
+            .add(keeperBalance[_user])
+            .sub(keeperPayouts[_user]);
+    }
+
+    function getNow()
+        public
+        view
+        returns (uint256)
+    {
+        return block.timestamp;
+    }
+
+    // for testing (consider removing)
+    function changeDistributionToken(
+        address _newDistributionToken
     )
         external
+        onlyCEO
     {
-        for(uint i = 0; i < _recipients.length; i++) {
-            distributeTokens(_recipients[i]);
-        }
-    }
-
-    function distributeTokens(
-        address _recipient
-    )
-        public
-        returns (uint256 tokenAmount)
-    {
-        require(
-            distributionEnabled == true,
-            'Airdrop: distribution disabled'
+        distributionToken = ERC20Token(
+            _newDistributionToken
         );
-        tokenAmount = airdropBalance[_recipient];
-        airdropBalance[_recipient] = 0;
-        distributionToken.transfer(_recipient, tokenAmount);
-    }
-
-    function changeDistributionToken(address _newDistributionToken) external onlyCEO {
-        distributionToken = ERC20Token(_newDistributionToken);
-    }
-
-    function enableDistribtion(bool _state) external onlyCEO {
-        distributionEnabled = _state;
-    }
-
-    function withdrawTokens(uint256 _amount) external onlyCEO {
-        distributionToken.transfer(msg.sender, _amount);
     }
 }
