935b76da53fcbda5c92a4ff82ccca7e97c6d60d9
0xsaxit <bcsaxit@gmail.com>
2021-04-15 11:10:09 +0700

    flat file update dgPointer (v3)

--- flats/dgPointerFlat.sol
+++ flats/dgPointerFlat.sol
@@ -239,6 +239,7 @@ interface ERC20Token {
     function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function decimals() external returns (uint8);
 
     event Transfer(
         address indexed from,
