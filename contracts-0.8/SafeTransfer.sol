f9616d04ab184dbc8c8ff6514721b41d8b4a3243
0xsaxit <bcsaxit@gmail.com>
2021-10-04 00:17:27 +0700

    safeBalanceOf added

--- contracts-0.8/SafeTransfer.sol
+++ contracts-0.8/SafeTransfer.sol
@@ -32,3 +32,27 @@ function safeTransfer(
         'TransferHelper: TRANSFER_FAILED'
     );
 }
+
+bytes4 constant BALANCE_OF = bytes4(
+    keccak256(
+        bytes(
+            'balanceOf(address)'
+        )
+    )
+);
+
+function safeBalanceOf(
+    address _token,
+    address _owner
+)
+    returns (uint256)
+{
+    (bool success, bytes memory data) = _token.call(
+        abi.encodeWithSelector(
+            BALANCE_OF,
+            _owner
+        )
+    );
+    if (!success) return 0;
+    return abi.decode(data, (uint256));
+}
--- contracts-0.8/iceToken.sol
+++ contracts-0.8/iceToken.sol
@@ -246,8 +246,18 @@ contract IceToken is EIP712MetaTransaction {
     function _msgSender()
         internal
         view
-        returns (address)
+        returns(address sender)
     {
-        return msg.sender;
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(mload(add(array, index)), 0xffffffffffffffffffffffffffffffffffffffff)
+            }
+        } else {
+            sender = msg.sender;
+        }
+        return sender;
     }
 }
