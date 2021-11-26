ef498051b17897dd1c39c9e8eb021888be67ef84
0xsaxit <bcsaxit@gmail.com>
2021-11-26 22:23:43 +0700

    coverage report generated

--- truffle-config.js
+++ truffle-config.js
@@ -1,63 +1,44 @@
-
-const PrivateKeyProvider = require("truffle-privatekey-provider");
-const privateKey = 'C7F038F9A424D7604EE05A53FED9920F4859C4D283ED9385B93913C13266571C';
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const mnemonic = 'insect minimum meadow eight hard voyage buzz cotton shrimp time vague banana';
+// const infura_id = '68e794f7333f47c2855ee7491aefeef4';
 
 module.exports = {
-    compilers: {
-        solc: {
-            version: "0.8.9",
-            settings: {
-                optimizer: {
-                    enabled: true,
-                    runs: 300
-                }
-            },
-        }
-    },
+    plugins: ["solidity-coverage"],
     networks: {
         development: {
             host: "127.0.0.1",
             port: 9545,
+            gasLimit: 8000000,
             network_id: 5777
         },
-        matic: {
-            provider: () => new PrivateKeyProvider(
-                privateKey,
-                'https://testnetv3.matic.network'
-            ),
-            network_id: 15001,
-            gasPrice: '0x0',
-            confirmations: 2,
-            timeoutBlocks: 200,
+        /* rinkeby: {
+            provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infura_id}`),
+            network_id: 4,
             skipDryRun: true
         },
-        mumbai: {
-            provider: () => new PrivateKeyProvider(
-                privateKey,
-                'https://rpc-mumbai.matic.today'
-            ),
-            network_id: 80001,
-            confirmations: 1,
-            timeoutBlocks: 300,
+        ropsten: {
+            provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infura_id}`),
+            network_id: 3,
             skipDryRun: true
-        },
-        maticmain: {
-            provider: () => new PrivateKeyProvider(
-                privateKey,
-                'https://rpc-mainnet.matic.network'
-            ),
-            network_id: 137,
-            confirmations: 1,
-            timeoutBlocks: 300,
-            skipDryRun: true
-        }
+        }*/
     },
     mocha: {
         useColors: true,
         reporter: "eth-gas-reporter",
         reporterOptions: {
             currency: "USD",
-            gasPrice: 10
+            gasPrice: 5
+        }
+    },
+    compilers: {
+        solc: {
+            version: "^0.8.9",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                },
+            }
         }
     }
 };
